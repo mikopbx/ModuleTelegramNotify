acbf88cf194933b489fe42ed2310376ea372c9ee
Nikolay Beketov <nbek@miko.ru>
2021-11-19 13:11:12 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/de/

--- Messages/de.php
+++ Messages/de.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'Token nicht gefüllt',
+    'mod_tgm_Token' => 'Telegramm-Bot-Token',
+    'SubHeaderModuleTelegramNotify' => 'Chat-Benachrichtigungen / Arbeit mit Anrufverlauf.',
+    'BreadcrumbModuleTelegramNotify' => 'Telegrammintegrationsmodul',
+    'mo_ModuleTelegramNotify' => 'Telegrammintegrationsmodul',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Telegrammintegrationsmodul -% Repräsentant%',
+];

cf6ab5a4c97bc8cd7ab58711b3a49b1fd072c7d2
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:27:54 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'ჟეტონი არ არის შევსებული',
+    'mod_tgm_Token' => 'Telegram ბოტის ჟეტონი',
+    'SubHeaderModuleTelegramNotify' => 'ჩეთის შეტყობინებები / მუშაობა ზარების ისტორიასთან.',
+    'BreadcrumbModuleTelegramNotify' => 'Telegram-ის ინტეგრაციის მოდული',
+    'mo_ModuleTelegramNotify' => 'Telegram-ის ინტეგრაციის მოდული',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Telegram ინტეგრაციის მოდული -% repesent%',
+];

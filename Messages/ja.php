689240252f4471b131fe315c5505df22ca5d6e3e
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:25:17 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/ja/

--- Messages/ja.php
+++ Messages/ja.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'トークンが入力されていません',
+    'mod_tgm_Token' => 'テレグラムボットトークン',
+    'SubHeaderModuleTelegramNotify' => 'チャット通知/通話履歴の操作。',
+    'BreadcrumbModuleTelegramNotify' => 'テレグラム統合モジュール',
+    'mo_ModuleTelegramNotify' => 'テレグラム統合モジュール',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'テレグラム統合モジュール-％repesent％',
+];

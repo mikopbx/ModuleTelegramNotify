6b6738993b277e0e50be1c3d20a3af964ccd0af3
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:21:54 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'Token không được lấp đầy',
+    'mod_tgm_Token' => 'Mã thông báo bot Telegram',
+    'SubHeaderModuleTelegramNotify' => 'Thông báo trò chuyện / làm việc với lịch sử cuộc gọi.',
+    'BreadcrumbModuleTelegramNotify' => 'Mô-đun tích hợp Telegram',
+    'mo_ModuleTelegramNotify' => 'Mô-đun tích hợp Telegram',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Mô-đun tích hợp Telegram -% repesent%',
+];

d93f6ab66654788eb09eb06446034dd38a554ac2
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:24:11 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/uk/

--- Messages/uk.php
+++ Messages/uk.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'Не заповнений токен',
+    'mod_tgm_Token' => 'Токен телеграм бота',
+    'SubHeaderModuleTelegramNotify' => 'Оповіщення в чаті / робота з історією дзвінків.',
+    'BreadcrumbModuleTelegramNotify' => 'Модуль інтеграції з Telegram',
+    'mo_ModuleTelegramNotify' => 'Модуль інтеграції з Telegram',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Модуль інтеграції з Telegram - %repesent%',
+];

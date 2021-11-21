b7b11309f534704b6f5f51a96e3686bf19d1f6be
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:37:54 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/en/

--- Messages/en.php
+++ Messages/en.php
@@ -1,16 +1,16 @@
 <?php
-/**
+return [
+    /**
  * Copyright © MIKO LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Alexey Portnov, 12 2018
  */
-
-return [
-    'repModuleTelegramNotify'        => 'Module telegram bot - %repesent%',
-    'mo_ModuleModuleTemplate'        => 'Module telegram bot',
+    'repModuleTelegramNotify' => 'Module telegram bot - %repesent%',
+    'mo_ModuleModuleTemplate' => 'Module telegram bot',
     'BreadcrumbModuleTelegramNotify' => 'The Telegram messenger integration module',
-    'SubHeaderModuleTelegramNotify'  => 'Call notifications, callback, access to the calls history',
-    'mod_tgm_Token'                  => 'The Telegram bot token',
-    'mod_tgm_ValidateTokenEmpty'     => 'Token is empty',
+    'SubHeaderModuleTelegramNotify' => 'Call notifications, callback, access to the calls history',
+    'mod_tgm_Token' => 'The Telegram bot token',
+    'mod_tgm_ValidateTokenEmpty' => 'Token is empty',
+    'mo_ModuleTelegramNotify' => 'The Telegram messenger integration module',
 ];

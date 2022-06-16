3773bfee5a54771ae94105aacb809dac10bde67d
Voutsas Theocharis <voutsasth@yahoo.gr>
2022-06-16 19:26:22 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/el/

--- Messages/el.php
+++ Messages/el.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'Μη συμπληρωμένο διακριτικό',
+    'mod_tgm_Token' => 'Token για το Telegram',
+    'SubHeaderModuleTelegramNotify' => 'Ειδοποιήσεις συνομιλίας / εργασία με ιστορικό κλήσεων.',
+    'BreadcrumbModuleTelegramNotify' => 'Μονάδα ενσωμάτωσης Telegram',
+    'mo_ModuleTelegramNotify' => 'Μονάδα ενσωμάτωσης Telegram',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Μονάδα ενσωμάτωσης Telegram - %repesent%',
+];

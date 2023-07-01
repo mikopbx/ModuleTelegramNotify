2c74a270c76148b42c3a82fd53366b8aa713189c
Jochem Pluim <jochem@cafedoranjeboom.nl>
2023-07-01 23:13:38 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'Token is leeg',
+    'mod_tgm_Token' => 'Telegram bot-token',
+    'SubHeaderModuleTelegramNotify' => 'Gespreksnotificaties, terugbelverzoeken en toegang tot de gesprekshistorie.',
+    'BreadcrumbModuleTelegramNotify' => 'Telegram integratie',
+    'mo_ModuleTelegramNotify' => 'Telegram integratie',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Module Telegram Bot - %repesent%',
+];

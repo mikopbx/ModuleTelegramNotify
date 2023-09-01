51a0a10758232e346072bd00f823358ef0945f8c
Anonymous <noreply@weblate.org>
2023-09-01 12:01:23 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/az/

--- Messages/az.php
+++ Messages/az.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Telegram inteqrasiya modulu - %repesent%',
+    'BreadcrumbModuleTelegramNotify' => 'Telegram inteqrasiya modulu',
+    'mo_ModuleTelegramNotify' => 'Telegram inteqrasiya modulu',
+    'SubHeaderModuleTelegramNotify' => 'Söhbət xəbərdarlığı / zəng tarixçəsi ilə işləmək.',
+    'mod_tgm_Token' => 'Telegram bot nişanı',
+    'mod_tgm_ValidateTokenEmpty' => 'Token doldurulmayıb',
+];

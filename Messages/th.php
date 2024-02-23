ead26f99a648a5f7f26cdbc6e7c9084c0cf4dd8f
Anonymous <noreply@weblate.org>
2024-02-23 02:57:36 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/th/

--- Messages/th.php
+++ Messages/th.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'โมดูลการรวมโทรเลข - % ซ้ำ%',
+    'mo_ModuleTelegramNotify' => 'โมดูลการรวมโทรเลข',
+    'BreadcrumbModuleTelegramNotify' => 'โมดูลการรวมโทรเลข',
+    'SubHeaderModuleTelegramNotify' => 'การแจ้งเตือนการแชท/การทำงานกับประวัติการโทร',
+    'mod_tgm_Token' => 'โทเค็นบอทโทรเลข',
+    'mod_tgm_ValidateTokenEmpty' => 'โทเค็นไม่ได้กรอก',
+];

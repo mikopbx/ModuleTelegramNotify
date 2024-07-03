15d22cc40856aaa695831b7ac799d623c85ad947
David Coimbra de Oliveira <microadsz@gmail.com>
2024-07-03 20:11:39 +0000

    Translated using Weblate (glossary (generated) (glossary))
    Currently translated at 16.6% (1 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/glossary/

--- Messages/glossary.php
+++ Messages/glossary.php
@@ -1,1 +1,10 @@
 <?php
+return [
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Módulo de integração do Telegram - %repesent%',
+];

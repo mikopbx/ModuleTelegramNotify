edac93614822f600b10509569a6d3c18276a2b97
Anonymous <noreply@weblate.org>
2024-07-30 07:27:12 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'BreadcrumbModuleTelegramNotify' => 'Telegram integrációs modul',
+    'SubHeaderModuleTelegramNotify' => 'Csevegési értesítések/munka a híváselőzményekkel.',
+    'mod_tgm_Token' => 'Telegram bot token',
+    'mod_tgm_ValidateTokenEmpty' => 'A token nincs kitöltve',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Telegram integrációs modul – %repesent%',
+    'mo_ModuleTelegramNotify' => 'Telegram integrációs modul',
+];

3f357934367dd561d6cf704e7a11b8ac1a0e22c0
Anonymous <noreply@weblate.org>
2024-09-06 04:01:25 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/fi/

--- Messages/fi.php
+++ Messages/fi.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Telegramin integrointimoduuli - %repesent%',
+    'mo_ModuleTelegramNotify' => 'Telegramin integrointimoduuli',
+    'BreadcrumbModuleTelegramNotify' => 'Telegramin integrointimoduuli',
+    'SubHeaderModuleTelegramNotify' => 'Chat-ilmoitukset / työskentely puheluhistorian kanssa.',
+    'mod_tgm_Token' => 'Telegram-bot-tunnus',
+    'mod_tgm_ValidateTokenEmpty' => 'Tunnusta ei ole täytetty',
+];

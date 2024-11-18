af40d3211252aecb569fab1980a6b8c63ccecd1e
Anonymous <noreply@weblate.org>
2024-11-18 23:03:40 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MIKOPBX/ModuleTelegramNotify
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduletelegramnotify/hr/

--- Messages/hr.php
+++ Messages/hr.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'mod_tgm_ValidateTokenEmpty' => 'Token nije popunjen',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 12 2018
+ */
+    'repModuleTelegramNotify' => 'Integracijski modul Telegrama - %repesent%',
+    'mo_ModuleTelegramNotify' => 'Telegram integracijski modul',
+    'BreadcrumbModuleTelegramNotify' => 'Telegram integracijski modul',
+    'SubHeaderModuleTelegramNotify' => 'Obavijesti o chatu/rad s poviješću poziva.',
+    'mod_tgm_Token' => 'Telegram bot token',
+];
